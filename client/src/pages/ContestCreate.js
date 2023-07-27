ab6d0975fdf05b69ba78c1fe361bef4b325e23db
shimucse <abisheema@gmail.com>
2023-07-27 16:04:02 +0600

    made a timmer

--- client/src/pages/ContestCreate.js
+++ client/src/pages/ContestCreate.js
@@ -10,32 +10,26 @@ import { useLocation} from "react-router-dom";
 import CountdownTimer from './contestTimer/CountdownTimer';
 
 
-const Home = ()=>{
+const ContestCreate = ()=>{
 
       let navigate = useNavigate();
       const[token, setToken] = useState('');
 
-      const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
-       const NOW_IN_MS = new Date().getTime();
-
-        const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+    
     useEffect(
         ()=>{
               setToken(localStorage.getItem('token'));
             }
     ,[]);
     console.log("token:"+token);
-  //  <CountdownTimer targetDate={dateTimeAfterThreeDays} />
 
 
   return (
       <>
                
       <div className="H_wrap">
-          <div className="H_body_column">             
-                 <button   className="ContestHistory">Contest history</button>
-                 <button   className="ContestHistory">timer</button>
-                 <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+          <div className="H_body_column">          
+                 <CountdownTimer />
 
 
          </div>
@@ -48,4 +42,4 @@ const Home = ()=>{
     
   );
 }
-export default Home;
+export default ContestCreate;
--- client/src/pages/contestTimer/CountdownTimer.js
+++ client/src/pages/contestTimer/CountdownTimer.js
@@ -1,55 +1,111 @@
+import React, { useState, useEffect } from "react";
+import Timer from "./Timer";
 
-import React from 'react';
-import DateTimeDisplay from './DateTimeDisplay';
-import useCountdown from 'react-use-countdown';
+//import { BsFillPlayFill, BsPauseFill, BsStopFill } from "react-icons/bs";
 
+document.body.style.background = "#282c34";
+export default function CountdownTimer() {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(null);
+  // End of Time
 
-const ExpiredNotice = () => {
+  const [showEndScreen, setShowEndScreen] = useState({
+    show: false,
+    message: "Happy coding in 2023",
+  });
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+         if (seconds > 0) {
+          setSeconds((seconds) => seconds - 1);
+        } else if (minutes > 0) {
+          setMinutes((minutes) => minutes - 1);
+          setSeconds(59);
+        } else if (hours > 0) {
+          setHours((hours) => hours - 1);
+          setMinutes(59);
+          setSeconds(59);
+        }
+      }, 10);
+    }
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      setShowEndScreen({ ...showEndScreen, show: true });
+      resetTimer();
+    }
+    return () => clearInterval(interval);
+  }, [seconds, minutes, hours, isRunning, showEndScreen.show]);
+
+  // Start Pause & Stop functions
+
+  // Start
+  function startTimer() {
+    if (hours !== 0 || minutes !== 0 || seconds !== 0) {
+      setIsRunning(true);
+      setShowEndScreen({ ...showEndScreen, show: false });
+    } else {
+      window.alert("Add Time.");
+    }
+  }
+
+  // Pause
+  function pauseTimer() {
+    setIsRunning(false);
+  }
+  // Stop
+
+  function stopTimer() {
+    resetTimer();
+    setShowEndScreen({ ...showEndScreen, show: false });
+  }
+
+  function resetTimer() {
+    setIsRunning(false);
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+  }
+  // Handlers
+
+  const changeSeconds = (e) => {
+    setSeconds(e.target.value);
+  };
+  const changeMinutes = (e) => {
+    setMinutes(e.target.value);
+  };
+  const changeHours = (e) => {
+    setHours(e.target.value);
+  };
   return (
-    <div className="expired-notice">
-      <span>Expired!!!</span>
-      <p>Please select a future date and time.</p>
-    </div>
-  );
-};
-
-const ShowCounter = ({ days, hours, minutes, seconds }) => {
-  return (
-    <div className="show-counter">
-      <a
-        href="https://tapasadhikary.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="countdown-link"
-      >
-        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
-        <p>:</p>
-        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-        <p>:</p>
-        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-        <p>:</p>
-        <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-      </a>
-    </div>
-  );
-};
-
-const CountdownTimer = ({ targetDate }) => {
-  /*const [days, hours, minutes, seconds] = useCountdown(targetDate);
-
-  if (days + hours + minutes + seconds <= 0) {
-    return <ExpiredNotice />;
-  } else {
-    return (
-      <ShowCounter
-        days={days}
-        hours={hours}
-        minutes={minutes}
+    <div>
+      {showEndScreen.show && (
+        <h1 className="title  text-light">{showEndScreen.message}</h1>
+      )}
+      <Timer
         seconds={seconds}
+        minutes={minutes}
+        hours={hours}
+        changeSeconds={changeSeconds}
+        changeMinutes={changeMinutes}
+        changeHours={changeHours}
       />
-    );
-  }*/
-};
-
-
-export default CountdownTimer;
+      <br />
+      {!isRunning && (
+        <button className="btn btn-accept btn-lg" onClick={startTimer}>
+         Play
+        </button>
+      )}
+      {isRunning && (
+        <button className="btn btn-warning btn-lg" onClick={pauseTimer}>
+          pause
+        </button>
+      )}{" "}
+      <button className="btn btn-danger btn-lg" onClick={stopTimer}>
+         Stop
+      </button>
+    </div>
+  );
+}
