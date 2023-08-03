470a6ced203242387b2cbd49319db43c76f023b1
shimucse <abisheema@gmail.com>
2023-08-03 12:09:18 +0600

    DateRemaining

--- client/src/pages/contest/ContestCreate.js
+++ client/src/pages/contest/ContestCreate.js
@@ -1,39 +1,53 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter as Router,  Route, Redirect,Routes} from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
 import './css/ContestCreate.css'
-import { useLocation} from "react-router-dom";
 import CountdownTimer from './CountdownTimer';
-import './css/ContestCreate.css'
-import DatePicker from "react-datepicker";  
-import "react-datepicker/dist/react-datepicker.css";  
+
+import DatePicker from 'react-datepicker';  
 import addDays from 'date-fns/addDays'  
+import "react-datepicker/dist/react-datepicker.css";  
+
 
 
 const ContestCreate = ()=>{    
 
   const [startDate, setStartDate] = useState(new Date());  
+  const [ RemainingDays, setRemainingDays] = useState(' ');
+
   let dateDifferent = ()=>{
-     console.log(new Date())
+     console.log("new Date "+new Date());
+     console.log("new Date "+ startDate);
+     const timeDifference = Math.abs(startDate-new Date());
+     const daysRemaining =Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+     console.log("daysRemaining"+daysRemaining);
+     setRemainingDays(daysRemaining);
   }
+
+  
 
   return (
       <>
                
       <div className="wrap">
           <div className="body_column">  
+            <div className="myContainter">
+               <label className="dateTimeSelector">Select Date and Time </label>
+                <DatePicker  
 
-
-               <DatePicker 
-                   selected={startDate}
-                   onChange={(date) =>   
-                   setStartDate(date)} 
-                   minDate={new Date()}
-                   maxDate={addDays(new Date(),15)}
-                />   
+                     className="myDatePicker"
+                     selected={ startDate}  
+                     onChange={(date) =>   
+                     setStartDate(date)} 
+                     showTimeSelect  
+                     timeFormat="HH:mm"  
+                     timeIntervals={20}  
+                     timeCaption="time"  
+                     dateFormat="MMMM d, yyyy h:mm aa"  
+                     minDate={new Date()}  
+                     maxDate={addDays(new Date(), 7)}  
+               />  
+                </div> 
+  
                 <br />
                 <button onClick={dateDifferent}>different date</button>
                 <br />      
